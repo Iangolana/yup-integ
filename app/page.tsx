479bdcdc9ee94fbbe6b2397d47aa4b41e9f55a35
Iangolana <iangolana@hairun-technology.com>
2023-02-02 18:32:44 +0300

    :construction: add .next/ in gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <div className="bg-primary-orange">
         <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-        <p className="text-3xl font-bold underline">Kaiza e</p>
+        <p className="text-3xl font-bold">Kaiza e</p>
       </div>
     </main>
   );
