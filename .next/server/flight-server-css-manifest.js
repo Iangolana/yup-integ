9366a8881ebf6e0d8c68ad48faad55973a7dd5a8
odilon <odilon@hairun-technology.com>
2023-02-02 10:30:14 +0300

    test import image

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,10 +1,10 @@
 self.__RSC_CSS_MANIFEST={
   "__entry_css_mods__": {
-    "C:\\wamp64\\www\\js\\yupwego\\yup-integ\\app\\page": [
-      "C:\\wamp64\\www\\js\\yupwego\\yup-integ\\styles\\globals.css"
+    "D:\\Projets\\yup-integ\\app\\page": [
+      "D:\\Projets\\yup-integ\\styles\\globals.css"
     ]
   },
-  "C:\\wamp64\\www\\js\\yupwego\\yup-integ\\app\\layout.tsx": [
-    "C:\\wamp64\\www\\js\\yupwego\\yup-integ\\styles\\globals.css"
+  "D:\\Projets\\yup-integ\\app\\layout.tsx": [
+    "D:\\Projets\\yup-integ\\styles\\globals.css"
   ]
 }
