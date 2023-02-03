3613bba4e0efa6ca93d30b97591eedf2faa30b5e
odilon <odilon@hairun-technology.com>
2023-02-03 09:08:11 +0300

    add component GoodHands

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import GoodHands from "@/components/GoodHands/page";
 import Search from "@/components/Home/Search/page";
 
 export default function Home() {
   return (
-    <div id="search" className="h-screen p-4 absolute top-0 w-full">
-      <Search />
-    </div>
+    <>
+      <div id='search' className='h-screen p-4 absolute top-0 w-full'>
+        <Search />
+      </div>
+      <GoodHands />
+    </>
   );
 }
