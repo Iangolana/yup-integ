135115acf557aa21a8db46e47841fe34de7312d8
odilon <odilon@hairun-technology.com>
2023-02-03 17:09:51 +0300

    amelioration page good hands

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Search from "@/components/Home/Search/page";
 export default function Home() {
   return (
     <>
-      <div id='search' className='h-screen p-4 absolute top-0 w-full'>
-        <Search />
+      <div className='fullpage'>
+        <div id='search' className='h-screen p-4 absolute top-0 w-full'>
+          <Search />
+        </div>
+        <GoodHands />
       </div>
-      <GoodHands />
     </>
   );
 }
