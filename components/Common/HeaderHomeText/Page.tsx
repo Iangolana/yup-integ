c814f6c2cbb909d623421d899bef156fb795be49
odilon <odilon@hairun-technology.com>
2023-02-07 18:06:06 +0300

    responsive header

---
+++ components/Common/HeaderHomeText/Page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const HeaderHomeText = () => {
+  return (
+    <div className='text-xs leading-4 max-w-[274px] py-6'>
+      Moderne. Personnalisée. Au juste prix. Sans limite. Il était temps qu’on
+      vienne vous assurer !
+    </div>
+  );
+};
+
+export default HeaderHomeText;
