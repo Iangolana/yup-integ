88a48256f526ee094191f9019fafc9648901fab5
odilon <odilon@hairun-technology.com>
2023-02-07 13:56:05 +0300

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,16 +35,13 @@ module.exports = {
       backgroundImage: {
         mask1: "url('../public/images/mask1.svg')",
         masque1: "url('../public/images/masque1.svg')",
-        mask2: "url('../public/images/test.svg')",
+        mask2: "url('../public/images/mask2.svg')",
+        mask2mobile: "url('../public/images/mask2mobile.svg')",
       },
       backgroundPosition: {
         mask1: "left bottom",
         masque: "right bottom",
       },
-    },
-    screens: {
-      lg: "1440px",
-      "3xl": "1600px",
     },
   },
 };
