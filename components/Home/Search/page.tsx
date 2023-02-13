e12494cf236990c8052dc015d5ea90b6acc6e3c0
olivierhrt <olivier@hairun-technology.com>
2023-02-13 10:13:02 +0300

    fix bug responsive

--- components/Home/Search/page.tsx
+++ components/Home/Search/page.tsx
@@ -6,40 +6,39 @@ import Logo from "@/components/icons/Logo";
 import ForThePlanetButton from "@/components/Common/Button/ForThePlanetButton/page";
 
 import Image from "next/legacy/image";
+import HeaderHomeTitle from "@/components/Common/HeaderHomeTitle/Page";
 import HeaderHomeText from "@/components/Common/HeaderHomeText/Page";
+import FormSearchMobile from "@/components/Common/FormSearchMobile/page";
 
 const Search = () => {
   return (
     <>
-      <div className="absolute image-wrapper h-screen w-screen overflow-hidden">
+      <div className='absolute image-wrapper h-screen w-screen'>
         <Image
-          src="/images/park.jpg"
-          width="1920"
-          height="1280"
-          objectFit="cover"
+          src='/images/park.jpg'
+          width='1920'
+          height='1280'
+          objectFit='cover'
         />
-        <div className="absolute w-full h-full z-9 top-0 left-0 mask-wrapper after:content-[' '] after:inline-block after:w-full after:h-full after:absolute after:bg-masque1 after:bg-position-mask1 after:bg-no-repeat after:bg-cover"></div>
+        <div className="hidden lg:block absolute w-full h-full z-9 top-0 left-0 mask-wrapper after:content-[' '] after:inline-block after:w-full after:h-full after:absolute after:bg-masque1 after:bg-position-mask1 after:bg-no-repeat after:bg-cover"></div>
       </div>
 
-      <div className="ml-[91px] pt-6 flex flex-col h-full relative z-10">
-        <a href="#" title="homepage" className="inline-block w-fit">
+      <div className='lg:ml-[91px] lg:pt-6 flex flex-col h-full relative z-10'>
+        <a href='#' title='homepage' className='hidden lg:inline-block w-fit'>
           <Logo />
         </a>
-        <div className="relative grow flex flex-col justify-center w-full max-w-[calc(100%-223px)] mx-auto">
-          <h1 className="text-[50px] max-w-[450px] font-canelaText font-medium leading-[48px]">
-            L’assurance voyage{" "}
-            <span className="font-hurmeSans1 font-normal">proche de vous</span>
-          </h1>
-          <div className="text-xs leading-4 max-w-[274px] py-6">
-            Moderne. Personnalisée. Au juste prix. Sans limite. Il était temps
-            qu’on vienne vous assurer !
+        <div className='mt-[100px] lg:mt-0 relative grow flex flex-col justify-center w-full lg:max-w-[calc(100%-223px)] mx-auto'>
+          <HeaderHomeTitle />
+          <div className='hidden lg:block'>
+            <HeaderHomeText />
           </div>
 
-          <div className="form-search-wrapper">
+          <div className='hidden lg:block form-search-wrapper'>
             <FormSeach />
           </div>
+          <div className='block lg:hidden'>
+            <FormSearchMobile />
 
-          <div className="absolute z-20  right-4 -top-[4rem]">
             <div className='grid grid-cols-[20%_80%] grid-rows-2 lg:hidden mb-[39px] relative z-[0] mt-[-60px] auto-rows-min'>
               <div className='col-start-1 col-end-3 row-start-1 row-end-3 z-20 mt-[-12px] w-full min-h-[330px] relative'>
                 <Image
@@ -87,7 +86,7 @@ const Search = () => {
             <ForThePlanetButton />
           </div>
 
-          <div className="absolute z-20  w-fit inset-x-0 m-auto bottom-2">
+          <div className='absolute z-20  w-fit inset-x-0 m-auto bottom-2'>
             <ScrollButton />
           </div>
         </div>
